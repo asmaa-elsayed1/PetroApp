c2978082f6edf9cdfec0310f7a6848be2da28b62
asmaa elsayed <asmaa.el.ahmed95@gmail.com>
2023-12-27 01:27:44 +0200

    enhancements

--- src/test/java/tests/searchTest.java
+++ src/test/java/tests/searchTest.java
@@ -26,27 +26,17 @@ public class searchTest extends TestBase{
 	@Test(priority = 2)
 	    public void validateTableData() {
 	    List<WebElement> rows = test.table.findElements(By.tagName("tr"));
-	    // Get the last row
 	    WebElement lastRow = rows.get(rows.size() - 1);
-	    // Locate all cells (columns) within the last row
 	    List<WebElement> cells = lastRow.findElements(By.tagName("td"));
-
-	        // Ensure there is at least one row in the table
 	        if (rows.size() < 2) {
-	            // Handle the case where there are not enough rows (e.g., log a message, throw an exception)
 	            return;
 	        }
-	        // Extract data from each cell and do something with it
 	        String lastRowCarNumber = cells.get(0).getText();
 	        String lastRowFuelInGalon = cells.get(1).getText();
 	        String lastRowCompanyID = cells.get(5).getText();
-
-	        // Print or perform actions with the data
 	        System.out.println("Last Row - CarNumber: " + lastRowCarNumber +
 	                ", FuelInGalon: " + lastRowFuelInGalon +
 	                ", CompanyID: " + lastRowCompanyID);
-
-	        // Perform assertions
 	        AssertJUnit.assertEquals(lastRowCarNumber, "44444444");
 	        AssertJUnit.assertEquals(lastRowFuelInGalon, "40");
 	        AssertJUnit.assertEquals(lastRowCompanyID, "1010");
